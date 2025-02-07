c5a132a234fc97011f004f76e7c9d0245c9cfa12
Mohamad Abo Hilal <moabohilal@gmail.com>
2025-02-07 12:08:48 +0100

    fixed another issue

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import "./db/index.js";
 import express from "express";
 import errorHandler from "./middlewares/errorHandler.js";
-import pokemonRouter from "./routers/pokemonRouter.js";
 import userRouter from "./routers/userRouter.js";
-import leaderboardRouter from "./routers/leaderboardRouter.js";
 import cors from "cors";
 
 const app = express();
@@ -12,14 +10,8 @@ const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
-app.use("/pokemons", pokemonRouter);
 app.use("/users", userRouter);
-app.use("/leaderboard", leaderboardRouter);
 app.use("*", (req, res) => res.status(404).json({ error: "Not Found" }));
 app.use(errorHandler);
-
-app.use("/users", userRouter);
-app.use("/leaderboard", leaderboardRouter);
-app.use("*", (req, res) => res.status(404).json({ error: "Not Found" }));
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
